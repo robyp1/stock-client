9526033ea09fc2f954c13ceb6890b5a575864b14
Roby <roberto.portoni@gmail.com>
2019-12-21 19:03:17 +0100

    resolve bean configuration for externalize service WebClientStockClient

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module stock.client {
     requires org.apache.logging.log4j;
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires spring.context;
 
     exports com.mechanitis.demo.stockclient;
+    opens com.mechanitis.demo.stockclient to spring.core;
 }
